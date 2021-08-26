a9b6b66d2214692199e801a80ebed5c53445d07d
InoProGit <ignat.georgievich@gmail.com>
2021-08-26 19:10:53 +0500

    Added pages and global parts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FilterPageComponent } from './pages/filter-page/filter-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'filter', component: FilterPageComponent },
+  { path: 'country', component: CountryPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FilterPageComponent } from './pages/filter-page/filter-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { HeaderComponent } from './modules/global/header/header.component';
+import { FooterComponent } from './modules/global/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FilterPageComponent,
+    CountryPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
