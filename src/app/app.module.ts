e02e5359938829ccd1c389fe1ff80954faa04804
InoProGit <ignat.georgievich@gmail.com>
2021-08-31 17:09:00 +0500

    works whith neo4j, no graphQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { HeaderComponent } from './modules/global/header/header.component';
 import { FooterComponent } from './modules/global/footer/footer.component';
 
 import { FilterService } from './services/filter-service/filter.service';
+import {HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+
 
 @NgModule({
   declarations: [
@@ -22,10 +27,25 @@ import { FilterService } from './services/filter-service/filter.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
-    FilterService
+    FilterService,
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          method: 'POST',
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:7474/db/neo4j/tx', // Neo4j local
+            headers: new HttpHeaders().set('Authorization', 'Basic bmVvNGo6Z3JhcGhSb290'), // set autorization, `Basic ${btoa('neo4j:graphRoot')}`
+          }),
+        };
+      },
+      deps: [HttpLink],
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/filter-page/filter-page.component.ts
+++ src/app/pages/filter-page/filter-page.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+import { Apollo, gql } from 'apollo-angular';
+
+// We use the gql tag to parse our query string into a query document
+const GET_POSTS = gql`
+query {
+	Country {
+    name
+    # check the docs for more info
+  }
+}
+
+`;
+
 @Component({
   selector: 'app-filter-page',
   templateUrl: './filter-page.component.html',
@@ -7,9 +21,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterPageComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean = true;
+  countries: any;
 
-  ngOnInit(): void {
+  private querySubscription: Subscription = new Subscription;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: GET_POSTS
+    })
+      .valueChanges
+      .subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.countries = data.Countries;
+      });
+      console.log(this.countries);
+      this.test();
+  }
+
+  test() {
+    
+    let url = 'http://localhost:7474/db/neo4j/tx';
+    let username = 'neo4j';
+    let password = 'graphRoot';
+    let headers = new Headers();
+    let query = `
+    query {
+      Country {
+        name
+        # check the docs for more info
+      }
+    }
+    `;
+    // let base64 = require('base-64');
+
+
+    //headers.append('Content-Type', 'text/json');
+    // headers.append('Authorization', 'Basic ' + username + ":" + password);
+    // headers.append('query', 'query { Country {name} }');
+    headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+
+    fetch(url, {method:'POST',
+            headers: headers,
+            //credentials: 'user:passwd'
+          body: JSON.stringify({
+            query: 'query { Country {name} }'
+          }),
+          })
+    .then(response => response.json())
+    .then(json => console.log(json));
+  }
+
+
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
   }
 
 }
